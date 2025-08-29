ea27c375a051e8e3adeda67a4de1cfc14872c0c7
Tom Everson <studycafe.125@gmail.com>
2025-08-29 16:16:45 +0700

    Cloudflare Setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import react from "@astrojs/react";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }), icon(), react()]
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), icon(), react()],
+  output: "static",
+  adapter: cloudflare()
 });
